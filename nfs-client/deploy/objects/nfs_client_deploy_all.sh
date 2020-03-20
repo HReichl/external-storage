d8ce8ddaaf9ab0242af00c8ef43b23a7323d45e1
HReichl <31277938+HReichl@users.noreply.github.com>
2020-03-20 14:44:20 +0100

    Create all objects in project nfs-client-provisioner

--- nfs-client/deploy/objects/nfs_client_deploy_all.sh
+++ nfs-client/deploy/objects/nfs_client_deploy_all.sh
@@ -6,13 +6,13 @@ git checkout linuxacademy
 
 oc create -f project.yaml
 oc project nfs-client-provisioner
-oc create -f serviceaccount.yaml
-oc create -f clusterrole.yaml
-oc create -f role.yaml
-oc create -f clusterrolebinding.yaml
-oc create -f rolebinding.yaml
-oc create -f class.yaml
-oc adm policy add-scc-to-user hostmount-anyuid -z nfs-client-provisioner
+oc create -f serviceaccount.yaml -n nfs-client-provisioner
+oc create -f clusterrole.yaml -n nfs-client-provisioner
+oc create -f role.yaml -n nfs-client-provisioner
+oc create -f clusterrolebinding.yaml -n nfs-client-provisioner
+oc create -f rolebinding.yaml -n nfs-client-provisioner
+oc create -f class.yaml -n nfs-client-provisioner
+oc adm policy add-scc-to-user hostmount-anyuid -z nfs-client-provisioner -n nfs-client-provisioner
 sleep 10
-oc create -f deployment.yaml
+oc create -f deployment.yaml -n nfs-client-provisioner
 
